f2a2aecf15ab4cb9d1d64789b627c0f9290bda0b
Mike <Mdubbpro@gmail.com>
2018-04-07 23:46:03 -0700

    added warmups.

---
+++ warmup.js
@@ -0,0 +1,60 @@
+//1. check prime
+  // i = n
+  // o = bool
+//2. prime factors
+  // i = n
+  // o = array of int 
+//3. fibonacci
+  // i = n
+  // o = int
+//4. greatest common divisor
+  // i = a, b ints
+  // o = int
+//5. remove duplicate
+  // i = array of ints
+  // o = array of ints
+//6. merge two sorted arrays of ints
+  // i = a, b ints
+  // o = array of ints
+//7. swap two ints 
+  // i = a, b ints
+  // o = b, a ints
+  // c = zero space complexity
+//8. reverse string
+  // i = str
+  // o = str
+  // c = no .reverse
+//9. reverse words
+  // i = str
+  // o = str
+  // c = no .reverse
+//10. reverse words in their place
+  // i = str
+  // o = str
+//11. first non repeating char
+  // i = str
+  // o = char
+//12. remove duplicate char
+  // i = str
+  // o = str
+//13. check palindrome
+  // i = str
+  // o = bool
+//14. missing number
+  // i = array of ints
+  // o = int
+//15. possible sum of two
+  // i = array of ints, int k
+  // o = bool
+//16. largest sum
+  // i = array of ints
+  // o = int
+//17. counting zeros
+  // i = n 
+  // o = int
+//18. substring
+  // i = strings str and subStr, 
+  // o = bool
+//19. string permutations
+  // i = str
+  // o = array of str
